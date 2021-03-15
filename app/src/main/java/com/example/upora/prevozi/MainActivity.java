943b33a9750350ca74325ded59f178f9dd3b6aff
Janko Zakelšek <janko.zakelsek@gmail.com>
2021-03-15 22:51:37 +0100

    Driving v1.0

--- app/src/main/java/com/example/upora/prevozi/MainActivity.java
+++ app/src/main/java/com/example/upora/prevozi/MainActivity.java
@@ -14,43 +14,41 @@ import com.example.upora.data.Driving;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();  //se generira samo, za spodnji TAG
-    Driver jakob;
+    //Driver oseba;
     EditText etName;
     EditText etAge;
     EditText etTime;
+
     Driving driving;  //dodamo v Driving
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);    //objekti se generirajo
-        jakob = new Driver("jakob", 20, 55);
+        setContentView(R.layout.activity_main);    //objekti se sami generirajo, da ne pišemo na roke  //iz XML parsanje
+
+        // oseba = new Driver("jakob", 20, 55);
+        //  System.out.println(oseba);
+        //  Log.i(TAG, "Moj izpis: "+oseba.toString());   //ustvarim constraint field in MyActivity
 
         driving = new Driving("Bistrica");  //dodamo v Driving
 
-        System.out.println(jakob);
-        Log.i(TAG, "Moj izpis: "+jakob.toString());   //create constant field in Myactivity
         etName = findViewById(R.id.etName);
         etAge = findViewById(R.id.etAge);
         etTime = findViewById(R.id.etTime);
     }
 
-    public void onClickAdd(View view) {
-       // Log.i(TAG, etName.getText().toString());
-        //Log.i(TAG, etAge.getText().toString());
-       // String name = etName.getText().toString();
+    public void onClickAdd(View view) {   //dodam metodo
 
-       try {
-        Driver inserted = new Driver(etName.getText().toString(), Integer.parseInt(etAge.getText().toString()), Double.parseDouble(etTime.getText().toString()));
+        try {
+        Driver inserted = new Driver(etName.getText().toString(), Integer.parseInt(etAge.getText().toString()), Double.parseDouble(etTime.getText().toString()));   //branje vnosnih polj
 
            driving.add(inserted);
 
-           etName.setText("");
+           etName.setText("");    //pobriše vnosno polje
            etAge.setText("");
            etTime.setText("");
 
-
-           Log.i(TAG, "Inserted:" + inserted.toString());
+           Log.i(TAG, "Inserted:" + inserted.toString());     //izpis v Logcat -ni potrebno
            Log.i(TAG, "Driving Driver:" + driving.toString());
 
     } catch (Exception e){
